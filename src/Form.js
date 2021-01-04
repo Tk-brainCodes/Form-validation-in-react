bfd290629547254e575c727e7715709cc0ef720b
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-01-04 16:05:56 +0100

    added state

--- src/Form.js
+++ src/Form.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Form = () => {
+    const [name, SetName] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [error, setErrormessage] = useState('');
+
+
     return (
         <div className="Form__">
             <form>
-                <label>Username</label>
-                <input type="text" name="username" placeholder="Enter a username" />
-                <label>Password</label>
-                <input type="password" name="password" placeholder="Enter a password" />
-                <input type="email" name="email" placeholder="Enter your email" />
+                <label>Username:</label><br />
+                <input type="text" name="username" placeholder="Enter a username" /><br />
+                <label>Password:</label><br />
+                <input type="password" name="password" placeholder="Enter a password" /><br />
+                <label>Email:</label><br />
+                <input type="email" name="email" placeholder="Enter your email" /><br />
                 <input type="submit" />
             </form>
         </div>
