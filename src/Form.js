c1409428d4378f3490459de477a7341844125281
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-01-04 15:56:29 +0100

    created a form

--- src/Form.js
+++ src/Form.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Form = () => {
     return (
         <div className="Form__">
-            
+            <form>
+                <label>Username</label>
+                <input type="text" name="username" placeholder="Enter a username" />
+                <label>Password</label>
+                <input type="password" name="password" placeholder="Enter a password" />
+                <input type="email" name="email" placeholder="Enter your email" />
+                <input type="submit" />
+            </form>
         </div>
     )
 }
