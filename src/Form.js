486e6082ae09e879dbc51ab9b97364c9854f78cd
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-01-05 17:27:20 +0100

    added css file

--- src/Form.js
+++ src/Form.js
@@ -1,19 +1,66 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import './Form.css';
 
 const Form = () => {
-    const [name, SetName] = useState('');
-    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+    const [Password, setPassword] = useState('');
     const [email, setEmail] = useState('');
-    const [error, setErrormessage] = useState('');
+    const [error, setErrormessage] = useState(null);
+    const [passerror, setPassError] = useState(null);
 
+    let x = 'hey';
+    console.log(Number.isNaN(x))
 
+    const handleSubmit = () => {
+        if(!Password.password  || Password.password.length<6){
+          setPassError("password length must be up to 8");
+        } else { 
+            setPassError(null);
+            return false;
+        }
+
+        if (name === "") {
+            let err = "name cannot be blank";
+            setErrormessage(err);
+            return true;
+        } else {
+            setErrormessage(null)
+            return false;
+        }
+
+    }
+
+    const onChangeFunc = (e) => {
+        e.preventDefault();
+        setName(e.target.value)
+    }
     return (
         <div className="Form__">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Username:</label><br />
-                <input type="text" name="username" placeholder="Enter a username" /><br />
+
+                <input
+                    value={name}
+                    type="text"
+                    onChange={onChangeFunc}
+                    name="username"
+                    placeholder="Enter a username"
+                /><br />
+
+                {error && <strong className="error">{error}</strong>} <br />
+
                 <label>Password:</label><br />
-                <input type="password" name="password" placeholder="Enter a password" /><br />
+
+                <input
+                    value={Password}
+                    onChange={e => setPassword(e.target.value)}
+                    type="password"
+                    name="password"
+                    placeholder="Enter a password"
+                /><br />
+
+                {passerror && <strong className="error">{passerror}</strong>} <br />
+
                 <label>Email:</label><br />
                 <input type="email" name="email" placeholder="Enter your email" /><br />
                 <input type="submit" />
